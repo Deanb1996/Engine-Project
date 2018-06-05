b017ce4991b8cf4d631849390b2d917f394ee635
Dean <deanb1996@hotmail.co.uk>
2018-06-05 15:06:06 +0100

    Added Entity and IComponent headers

--- Game Engine/Game Engine/IComponent.h
+++ Game Engine/Game Engine/IComponent.h
@@ -1,14 +1,19 @@
-#pragma once
 class IComponent
 {
 public:
-	IComponent();
-	~IComponent();
-
-	enum ComponentMasks {
-		COMPONENT_NONE = 0
+	enum ComponentMasks
+	{
+		COMPONENT_NONE = 0,
+		COMPONENT_TRANSFORM = 1 << 0,
+		COMPONENT_GEOMETRY = 1 << 1,
+		COMPONENT_TEXTURE = 1 << 2,
+		COMPONENT_VELOCITY = 1 << 3,
+		COMPONENT_AI = 1 << 4,
+		COMPONENT_AUDIO = 1 << 5,
+		COMPONENT_SHADER = 1 << 6,
+		COMPONENT_HEALTH = 1 << 7
 	};
 
-	virtual void IComponent();
+	virtual ComponentMasks ComponentMask() = 0;
 };
 
---
+++ Game Engine/Game Engine/oEntity.h
@@ -0,0 +1,16 @@
+#ifndef ENTITY
+#define ENTITY
+
+#include "IComponent.h"
+#include <string>
+
+using namespace std;
+
+class oEntity
+{
+private:
+	string name;
+
+};
+
+#endif // !ENTITY
